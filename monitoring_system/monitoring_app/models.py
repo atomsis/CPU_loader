4ab58c69a5f1513ce4cea25dab379e188fd0fe95
atoms <atomsis01@mail.ru>
2024-05-01 15:17:21 +0300

    Всё работает-но bash скрипт работает только на  НЕ Windows

--- monitoring_system/monitoring_app/models.py
+++ monitoring_system/monitoring_app/models.py
@@ -1,17 +1,9 @@
 from django.db import models
-from datetime import timedelta
 
 
 class CPULoad(models.Model):
     load_percentage = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True,)
-
-    class Meta:
-        get_latest_by = 'timestamp'
-
-    def __str__(self):
-        return f'CPU={self.load_percentage},TIME={self.formatted_timestamp()}'
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def formatted_timestamp(self):
-        corrected_time = self.timestamp + timedelta(hours=3)
-        return corrected_time.strftime('%Y-%m-%d %H:%M:%S')
+        return self.timestamp.strftime('%Y-%m-%d %H:%M:%S')
--- monitoring_system/monitoring_app/serializers.py
+++ monitoring_system/monitoring_app/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import CPULoad
 
 class CPULoadSerializer(serializers.ModelSerializer):
-    timestamp = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     class Meta:
         model = CPULoad
-        fields = ['timestamp', 'load_percentage']
+        fields = '__all__'
