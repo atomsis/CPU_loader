b155420d0550959f312d66a8f4b14ed9c1e10b77
atoms <atomsis01@mail.ru>
2024-05-01 12:26:54 +0300

    Поместил js в отдельный файл и запустил чтобы работало

--- client_daemon.sh
+++ client_daemon.sh
@@ -3,23 +3,6 @@ SERVER_PORT="8001"
 
 while true; do
     cpuUsage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-#   cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
     curl -X POST -d "load_percentage=$cpuUsage" http://$SERVER_IP:$SERVER_PORT/api/cpu/load/
     sleep 10
 done
-
-#!/bin/bash
-# This script monitors CPU and memory usage
-
-#while true; do
-#  # Get the current usage of CPU and memory
-#  cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
-#  memUsage=$(free -m | awk '/Mem/{print $3}')
-#
-#  # Print the usage
-#  echo "CPU Usage: $cpuUsage%"
-#  echo "Memory Usage: $memUsage MB"
-#
-#  # Sleep for 1 second
-#  sleep 1
-#done
